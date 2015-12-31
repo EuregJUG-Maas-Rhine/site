9d73cda931679c9893551a9f11b5730c245c658f
Michael J. Simons <michael@simons.ac>
2015-12-31 12:40:00 +0100

    Use a tree map to sort archive

--- src/main/java/eu/euregjug/site/web/IndexController.java
+++ src/main/java/eu/euregjug/site/web/IndexController.java
@@ -27,11 +27,13 @@ import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
-import java.util.stream.Collectors;
+import java.util.TreeMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -39,7 +41,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import static java.util.Comparator.reverseOrder;
 import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
 
 /**
  * @author Michael J. Simons, 2015-12-27
@@ -113,8 +117,18 @@ class IndexController {
     }
     
     @RequestMapping({"/archive", "/archives"})
-    public String archive(final Model model) {
-	model.addAttribute("posts", this.postRepository.findAll().stream().map(Post::new).collect(Collectors.groupingBy(post -> post.getPublishedOn().withDayOfMonth(1))));
+    public String archive(final Model model) {	
+	model.addAttribute("posts", 
+		this.postRepository
+			.findAll(new Sort(Direction.DESC, "publishedOn")).stream()
+			.map(Post::new)
+			.collect(groupingBy(
+				    post -> post.getPublishedOn().withDayOfMonth(1), 
+				    () -> new TreeMap<LocalDate, List<Post>>(reverseOrder()), 
+				    toList()
+				)
+			)
+	);
 	return "archive";
     }
 }
