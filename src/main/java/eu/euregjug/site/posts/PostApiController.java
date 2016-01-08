cabaeab97db59ffee248fde9027e8fad45f64d62
Michael J. Simons <michael@simons.ac>
2016-01-08 12:50:22 +0100

    Make posts updatable

--- src/main/java/eu/euregjug/site/posts/PostApiController.java
+++ src/main/java/eu/euregjug/site/posts/PostApiController.java
@@ -15,17 +15,21 @@
  */
 package eu.euregjug.site.posts;
 
+import eu.euregjug.site.support.ResourceNotFoundException;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import static org.springframework.web.bind.annotation.RequestMethod.PUT;
 
 /**
  * @author Michael J. Simons, 2015-12-28
@@ -50,5 +54,19 @@ class PostApiController {
     @RequestMapping(method = GET)
     public Page<PostEntity> get(final Pageable pageable) {
 	return this.postRepository.findAll(pageable);
+    }
+    
+    @RequestMapping(value = "/{id:\\d+}", method = PUT)
+    @PreAuthorize("isAuthenticated()")
+    @Transactional
+    public PostEntity update(final @PathVariable Integer id, final @Valid @RequestBody PostEntity updatedPost) {
+	final PostEntity postEntity =  this.postRepository.findOne(id);
+	if(postEntity == null) {
+	    throw new ResourceNotFoundException();
+	}
+	postEntity.setContent(updatedPost.getContent());
+	postEntity.setFormat(updatedPost.getFormat());
+	postEntity.setTitle(updatedPost.getTitle());
+	return postEntity;	
     }
 }
