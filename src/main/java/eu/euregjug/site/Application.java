907c6d1e8adef30c6cf15471c90b84c8483248c3
Michael J. Simons <michael@simons.ac>
2016-07-07 09:24:13 +0200

    Enable @Async

--- src/main/java/eu/euregjug/site/Application.java
+++ src/main/java/eu/euregjug/site/Application.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 EuregJUG.
+ * Copyright 2015-2016 EuregJUG.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,12 +19,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * @author Michael J. Simons, 2015-12-26
  */
 @SpringBootApplication
 @EnableCaching
+@EnableAsync
 @PropertySource("classpath:build.properties")
 public class Application {
 
