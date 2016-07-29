52e69e34fdd4ca373405169fdb18ef87d4bc0822
Michael J. Simons <michael@simons.ac>
2016-07-29 14:29:09 +0200

    Add tests for IndexController and ICS view

---
+++ src/test/java/eu/euregjug/site/web/IndexControllerTest.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2016 EuregJUG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package eu.euregjug.site.web;
+
+import com.github.mkopylec.recaptcha.validation.RecaptchaValidator;
+import eu.euregjug.site.events.EventEntity;
+import eu.euregjug.site.events.EventRepository;
+import eu.euregjug.site.events.RegistrationService;
+import eu.euregjug.site.links.LinkRepository;
+import eu.euregjug.site.posts.PostRenderingService;
+import eu.euregjug.site.posts.PostRepository;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.GregorianCalendar;
+import java.util.List;
+import org.joor.Reflect;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+/**
+ *
+ * @author Michael J. Simons, 2016-07-17
+ */
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+@WebMvcTest(
+        controllers = IndexController.class,
+        secure = false,
+        // Need the custom view component as well...
+        includeFilters = @ComponentScan.Filter(classes = EventsIcalView.class, type = FilterType.ASSIGNABLE_TYPE)
+)
+@EnableSpringDataWebSupport // Needed to enable resolving of Pageable and other parameters
+@MockBean(classes = {
+    RegistrationService.class, 
+    LinkRepository.class, 
+    PostRepository.class,
+    PostRenderingService.class,
+    RecaptchaValidator.class
+})
+public class IndexControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private EventRepository eventRepository;
+
+    private final List<EventEntity> events;
+
+    public IndexControllerTest() {
+        final ZonedDateTime eventDate = ZonedDateTime.of(2016, 7, 7, 19, 0, 0, 0, ZoneId.of("Europe/Berlin"));
+        final EventEntity event1 = Reflect.on(
+                new EventEntity(GregorianCalendar.from(eventDate), "name-1", "desc-1")
+        ).set("id", 42).set("createdAt", GregorianCalendar.from(eventDate)).get();
+        event1.setDuration(60);
+
+        final ZonedDateTime eventDate2 = ZonedDateTime.of(2016, 11, 22, 18, 0, 0, 0, ZoneId.of("Europe/Berlin"));
+        final EventEntity event2 = Reflect.on(
+                new EventEntity(GregorianCalendar.from(eventDate2), "name-2", "desc-2")
+        ).set("id", 42).set("createdAt", GregorianCalendar.from(eventDate2)).get();
+        this.events = Arrays.asList(event1, event2);
+    }
+
+    @Test
+    public void eventsShouldWork() throws Exception {
+        when(this.eventRepository.findUpcomingEvents()).thenReturn(events);        
+        this.mvc
+                .perform(get("/events.ics").accept("text/calendar"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/calendar"))
+                .andExpect(content().string(""
+                        + "BEGIN:VCALENDAR\r\n"
+                        + "VERSION:2.0\r\n"
+                        + "PRODID:http://www.euregjug.eu/events\r\n"
+                        + "BEGIN:VEVENT\r\n"
+                        + "UID:42@euregjug.eu\r\n"
+                        + "ORGANIZER:EuregJUG\r\n"
+                        + "DTSTAMP:20160707T170000Z\r\n"
+                        + "DTSTART:20160707T170000Z\r\n"
+                        + "DTEND:20160707T180000Z\r\n"
+                        + "SUMMARY:name-1\r\n"
+                        + "DESCRIPTION:desc-1\r\n"
+                        + "END:VEVENT\r\n"
+                        + "BEGIN:VEVENT\r\n"
+                        + "UID:42@euregjug.eu\r\n"
+                        + "ORGANIZER:EuregJUG\r\n"
+                        + "DTSTAMP:20161122T170000Z\r\n"
+                        + "DTSTART:20161122T170000Z\r\n"
+                        + "DTEND:20161122T190000Z\r\n"
+                        + "SUMMARY:name-2\r\n"
+                        + "DESCRIPTION:desc-2\r\n"
+                        + "END:VEVENT\r\n"
+                        + "END:VCALENDAR\r\n"));
+
+        verify(this.eventRepository).findUpcomingEvents();
+        verifyNoMoreInteractions(this.eventRepository);
+    }
+
+}
