37bbfa84cf375a83c85128a8fa17f81e8e957631
Michael J. Simons <michael@simons.ac>
2018-05-22 11:50:30 +0200

    Add imprint

--- src/main/java/eu/euregjug/site/config/WebConfig.java
+++ src/main/java/eu/euregjug/site/config/WebConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2017 EuregJUG.
+ * Copyright 2015-2018 EuregJUG.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -38,6 +38,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(final ViewControllerRegistry registry) {
         registry.addViewController("/about").setViewName("about");
+        registry.addViewController("/imprint").setViewName("imprint");
     }
 
     @Bean
